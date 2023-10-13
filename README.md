c495c0fd4844fba92629e662fd8cc16cbd6b5d2e
Allen OYang <allenwill211@gmail.com>
2023-10-13 20:39:50 +0800

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  * @Author: Allen OYang
  * @Email:  allenwill211@gmail.com
  * @Date: 2023-09-05 11:44:53
- * @LastEditTime: 2023-09-14 18:11:52
+ * @LastEditTime: 2023-10-13 20:39:11
  * @LastEditors: Allen OYang allenwill211@gmail.com
  * @FilePath: /oin/README.md
 -->
@@ -10,3 +10,8 @@
 # ☠️☠️☠️ OIN ☠️☠️☠️
 
 TODO:
+登录：
+
+- [x] 手机号登录
+- [x] 邮箱登录
+- [ ] google 登录
--- apps/oin-service/src/app/modules/login/login.module.ts
+++ apps/oin-service/src/app/modules/login/login.module.ts
@@ -2,14 +2,13 @@
  * @Author: Allen OYang
  * @Email:  allenwill211@gmail.com
  * @Date: 2023-09-13 11:42:20
- * @LastEditTime: 2023-09-13 11:47:00
+ * @LastEditTime: 2023-10-13 17:55:54
  * @LastEditors: Allen OYang allenwill211@gmail.com
  * @FilePath: /oin/apps/oin-service/src/app/modules/login/login.module.ts
  */
 
 import { Module } from '@nestjs/common'
 import { AuthModule } from '@server/app/modules/auth/auth.module'
-
 import { LoginController } from './login.controller'
 import { LoginService } from './login.service'
 
